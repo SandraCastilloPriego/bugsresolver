12776508e55b2519bdd74e41e16dfff0734c3fb7
nolssi <nolssi@29731817-0dca-ed00-e3e5-5d32efdf5bda>
2010-10-31 11:00:06 +0000

    First classes

---
+++ BugsResolver/src/bugsresolver/data/impl/SimpleBasicDataset.java
@@ -0,0 +1,187 @@
+/*
+ * Copyright 2007-2010 VTT Biotechnology
+ * This file is part of Guineu.
+ *
+ * Guineu is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Guineu is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Guineu; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package bugsresolver.data.impl;
+
+import bugsresolver.data.BugDataset;
+import bugsresolver.data.PeakListRow;
+import java.util.Hashtable;
+import java.util.Vector;
+
+/**
+ * Basic data set implementation.
+ *
+ * @author SCSANDRA
+ */
+public class SimpleBasicDataset implements BugDataset {
+
+    String datasetName;
+    Vector<PeakListRow> peakList;
+    Vector<String> columnNames;
+    Hashtable<String, Integer> type;
+    String infoDataset = "";
+    private int ID;
+    private int numberRows = 0;
+
+    /**
+     *
+     * @param datasetName Name of the data set
+     */
+    public SimpleBasicDataset(String datasetName) {
+        this.datasetName = datasetName;
+        this.peakList = new Vector<PeakListRow>();
+        this.columnNames = new Vector<String>();
+        this.type = new Hashtable<String, Integer>();
+    }
+
+    /**
+     * Returns true when the list of column names contain the parameter <b>columnName</b>.
+     *
+     * @param columnName Name of the column to be searched
+     * @return true or false depending if the parameter <b>columnName</b> is in the name of any column
+     */
+    public boolean containtName(String columnName) {
+        for (String name : this.columnNames) {
+            if (name.compareTo(columnName) == 0) {
+                return true;
+            }
+            if (name.matches(".*" + columnName + ".*")) {
+                return true;
+            }
+            if (columnName.matches(".*" + name + ".*")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Returns true when any row of the data set contains the String <b>str</b> into a
+     * column called "Name"
+     *
+     * @param str
+     * @return true or false depending if the parameter <b>str</b> is in the column called "Name"
+     */
+    public boolean containRowName(String srt) {
+        for (PeakListRow row : this.getRows()) {
+            if (((String) row.getPeak("Name")).contains(srt) || srt.contains((CharSequence) row.getPeak("Name"))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getDatasetName() {
+        return this.datasetName;
+    }
+
+    public void setDatasetName(String datasetName) {
+        this.datasetName = datasetName;
+    }
+
+    public void addRow(PeakListRow peakListRow) {
+        this.peakList.addElement(peakListRow);
+    }
+
+    public void addColumnName(String nameExperiment) {
+        this.columnNames.addElement(nameExperiment);
+    }
+
+    public PeakListRow getRow(int i) {
+        return (PeakListRow) this.peakList.elementAt(i);
+    }
+
+    public Vector<PeakListRow> getRows() {
+        return this.peakList;
+    }
+
+    public int getNumberRows() {
+        return this.peakList.size();
+    }
+
+    public int getNumberRowsdb() {
+        return this.numberRows;
+    }
+
+    public void setNumberRows(int numberRows) {
+        this.numberRows = numberRows;
+    }
+
+    public int getNumberCols() {
+        return this.columnNames.size();
+    }
+
+    public Vector<String> getAllColumnNames() {
+        return this.columnNames;
+    }
+
+    public void setNameExperiments(Vector<String> experimentNames) {
+        this.columnNames = experimentNames;
+    }
+
+    public void removeRow(PeakListRow row) {
+        try {
+            this.peakList.removeElement(row);
+
+        } catch (Exception e) {
+            System.out.println("No row found");
+        }
+    }
+
+    public void addColumnName(String nameExperiment, int position) {
+        this.columnNames.insertElementAt(datasetName, position);
+    }
+
+    public String getInfo() {
+        return infoDataset;
+    }
+
+    public void setInfo(String info) {
+        this.infoDataset = info;
+    }
+
+    @Override
+    public SimpleBasicDataset clone() {
+        SimpleBasicDataset newDataset = new SimpleBasicDataset(this.datasetName);
+        for (String experimentName : this.columnNames) {
+            newDataset.addColumnName(experimentName);
+        }
+        for (PeakListRow peakListRow : this.peakList) {
+            newDataset.addRow(peakListRow.clone());
+        }
+
+        return newDataset;
+    }
+
+    public void setType(String sampleName, int type) {
+        this.type.put(sampleName, type);
+    }
+
+    public int getType(String sampleName) {
+        return type.get(sampleName);
+    }
+}
+
+       
---
+++ BugsResolver/src/bugsresolver/world/World.java
@@ -0,0 +1,183 @@
+/*
+ * Copyright 2010
+ * This file is part of XXXXXX.
+ * XXXXXX is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * 
+ * XXXXXX is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * XXXXXX; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package bugsresolver.world;
+
+import bugsresolver.data.BugDataset;
+import bugsresolver.data.PeakListRow;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Vector;
+
+/**
+ *
+ * @author bicha
+ */
+public class World {
+
+    BugDataset dataset;
+    Cell[][] cells;
+    List<Bug> population;
+    Random rand;
+    int cellsPerSide;
+    int numberOfCells;
+
+    public World(BugDataset dataset, int cellsPerSide) {
+        this.dataset = dataset;
+        this.cellsPerSide = cellsPerSide;
+        this.numberOfCells = cellsPerSide * cellsPerSide;
+        this.population = new ArrayList<Bug>();
+        this.rand = new Random();
+        if (dataset != null) {
+            cells = new Cell[cellsPerSide][cellsPerSide];
+            Vector<String> samplesNames = dataset.getAllColumnNames();
+            for (int i = 0; i < cellsPerSide; i++) {
+                cells[i] = new Cell[cellsPerSide];
+                for (int j = 0; j < cells[i].length; j++) {
+                    cells[i][j] = new Cell();
+                    this.setSamplesInCell(samplesNames, cells[i][j]);
+                }
+            }
+
+            for (PeakListRow row : dataset.getRows()) {
+                this.addBug(row);
+            }
+        }
+    }
+
+    private void setSamplesInCell(Vector<String> samplesNames, Cell cell) {
+        //60 % of the samples are for "training" and 40% for validation. The cells are the validation samples.
+        int numberForTraining = (int) (samplesNames.size() * 0.6);
+        int pos = rand.nextInt(samplesNames.size() - numberForTraining) + numberForTraining;
+        String name = samplesNames.elementAt(pos);
+        cell.setParameters(dataset.getType(name), name);
+    }
+
+    private void addBug(PeakListRow row) {
+        boolean isInside = true;
+        int cont = 0;
+        while (isInside) {
+            int X = this.rand.nextInt(this.cellsPerSide);
+            int Y = this.rand.nextInt(this.cellsPerSide);
+
+            Bug bug = new Bug(X, Y, cells[X][Y], row, dataset);
+            cells[X][Y].addBug(bug);
+            this.population.add(bug);
+            isInside = false;
+            cont++;
+            if (cont > numberOfCells) {
+                break;
+            }
+        }
+    }
+
+    public void cicle() {
+        movement();
+        eat();
+
+        for (Cell[] cellArray : cells) {
+            for (Cell cell : cellArray) {
+                Bug child = cell.reproduction();
+                if (child != null) {
+                    cell.addBug(child);
+                    this.population.add(child);
+                }
+            }
+        }
+
+        death();
+    }
+
+    private void movement() {
+        for (Bug bug : population) {
+            int direction = rand.nextInt(8);
+
+            int x = bug.getx();
+            int y = bug.gety();
+
+            switch (direction) {
+                case 0:
+                    this.setBugPosition(bug, x + 1, y);
+                    break;
+                case 1:
+                    this.setBugPosition(bug, x, y);
+                    break;
+                case 2:
+                    this.setBugPosition(bug, x, y + 1);
+                    break;
+                case 3:
+                    this.setBugPosition(bug, x, y - 1);
+                    break;
+                case 4:
+                    this.setBugPosition(bug, x + 1, y + 1);
+                    break;
+                case 5:
+                    this.setBugPosition(bug, x + 1, y - 1);
+                    break;
+                case 6:
+                    this.setBugPosition(bug, x - 1, y + 1);
+                    break;
+                case 7:
+                    this.setBugPosition(bug, x - 1, y - 1);
+                    break;
+            }
+
+        }
+    }
+
+    private void setBugPosition(Bug bug, int newx, int newy) {
+        if (newx > this.cellsPerSide - 1) {
+            newx = 1;
+        } else if (newx < 0) {
+            newx = this.cellsPerSide - 1;
+        }
+        if (newy > this.cellsPerSide - 1) {
+            newy = 1;
+        } else if (newy < 0) {
+            newy = this.cellsPerSide - 1;
+        }
+
+        bug.getCell().removeBug(bug);       
+        bug.setPosition(newx, newy);
+        cells[newx][newy].addBug(bug);
+        bug.setCell(cells[newx][newy]);
+
+    }
+
+    public List<Bug> getPopulation() {
+        return this.population;
+    }
+
+    public int getWorldSize() {
+        return this.cellsPerSide;
+    }
+
+    private void eat() {
+        for (Bug bug : population) {
+            bug.eat();
+        }
+    }
+
+    private void death() {
+        for (Bug bug : population) {
+            if (bug.isDead()) {
+                this.population.remove(bug);
+                this.cells[bug.getx()][bug.gety()].removeBug(bug);
+            }
+        }
+    }
+}
